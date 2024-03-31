fe5f2ab1e15961af192256674a9fe384e2550b7c
Crowdbotics <team@crowdbotics.com>
2024-03-31 10:57:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-waterfall-47332",
+  url: "https://shrill-waterfall-47332.botics.co",
+  api: "https://shrill-waterfall-47332.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
